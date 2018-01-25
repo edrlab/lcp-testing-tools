9418a7e7e7fbd2beb2381568f2b968b9b0b952ba
Rémi Bauzac <rbauzac@free.fr>
2018-01-25 16:17:40 +0100

    Add events checks

--- src/tests/test5/part2.py
+++ src/tests/test5/part2.py
@@ -9,20 +9,27 @@ class LCPTests(TestCase):
   def setUpClass(cls):
     cls.config = TestConfig('b3')
     cls.license = License(cls.config.license())
+    cls.status = Status(cls.license)
+    cls.status.update_status()
 
   def test_a_check_status_cancelled(self):  
-    """- Fetch the status document from this url and Check that the status is 'cancelled' """
-    status = Status(self.license)
-    status.update_status()
-    self.assertTrue(status.is_revoked())
+    """- Fetch the status document from this url and Check that the status is 'revoked' """
+    self.assertTrue(self.status.is_revoked())
  
   def test_b_check_revoked_event(self):
-    """- Test if a new 'cancelled' event appears in the status document, display the message """
-    pass
+    """- Test if a new 'revoke' event appears in the status document, display the message """
+    events = self.status.get_events()
+    if not events is None:
+      found = False
+      for event in self.status.get_events():
+        if event['type'] == self: 
+          found = True
+
+      self.assertTrue(found, "The cancel event is not available in the event list")
+
+
 
   def test_c_check_register_error(self):
     """- Check that a 'register' request returns an error """
-    status = Status(self.license)
-    status.update_status()
     with self.assertRaisesRegexp(IOError, 'POST .* HTTP error 400$'):
-      status.register(status.DEVICEID1, status.DEVICENAME1)
+      self.status.register(self.status.DEVICEID1, self.status.DEVICENAME1)
